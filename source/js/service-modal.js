374d75173d0c54184b5c869f45286d17abc9978c
Anton <kiyananton@gmail.com>
2021-12-06 13:01:48 +0300

    Добавляет облачный кондиционер

--- source/js/service-modal.js
+++ source/js/service-modal.js
@@ -3,34 +3,50 @@ const REG_EXP = {
   phone: /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/,
 }
 
-const $userInput = $('#user-input').parents('.input-block')
+const $select = $('#service-type')
+const $card = $('.service__card')
+const $userInput = $('#user-input')
+const $userMessage = $('#user-message')
+const $userInputBlock = $userInput.parents('.input-block')
+const $userInputMessage = $userMessage.parents('.input-block')
+const $submitBtn = $('.device-info__service-modal .modal__save')
 
-$('.service__card').on('click', function () {
-  $('#service-type').selectmenu('destroy')
+const clearFields = () => {
+  $userInput.val('')
+  $userMessage.val('')
+  $userInputBlock.removeClass('show-note js-error')
+  $userInputBlock.addClass('js-empty')
+  $userInputMessage.addClass('js-empty')
+  $submitBtn.attr('disabled', '')
+}
+
+$card.on('click', function () {
+  $select.selectmenu('destroy')
+  clearFields()
 
   const text = $(this).children('p').text()
-  $('#service-type option').removeAttr('selected')
-  $(`#service-type option:contains(${text})`).attr('selected', '')
 
-  $('#service-type').selectmenu()
+  $select.val(text).change()
+
+  $select.selectmenu()
 })
 
-$('#user-input').on('input', function () {
+$userInput.on('input', function () {
   $(this).val().trim()
-    ? $('.device-info__service-modal .modal__save').removeAttr('disabled')
-    : $('.device-info__service-modal .modal__save').attr('disabled', '')
+    ? $submitBtn.removeAttr('disabled')
+    : $submitBtn.attr('disabled', '')
 
-  $userInput.removeClass('show-note js-error')
+  $userInputBlock.removeClass('show-note js-error')
 })
 
-$('.device-info__service-modal .modal__save').on('click', function (e) {
+$submitBtn.on('click', function (e) {
   e.preventDefault()
 
   if (
-    $('#user-input').val().match(REG_EXP.email) === null &&
-    $('#user-input').val().match(REG_EXP.phone) === null
+    $userInput.val().match(REG_EXP.email) === null &&
+    $userInput.val().match(REG_EXP.phone) === null
   ) {
-    $userInput.addClass('show-note js-error')
+    $userInputBlock.addClass('show-note js-error')
   } else {
     console.log('success')
   }
